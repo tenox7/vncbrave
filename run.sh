2c3e28f2a24ef838bd3f0c4c7ecf9ab37ca805c8
Antoni Sawicki <as@tenoware.com>
2026-08-19 20:18:19 -0700

    fix profile<>hostname issues

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ docker run -d \
   --name vncbrave \
   -v vncbrave:/home/vncbrave/.config/BraveSoftware \
   -v /Volumes/Tmp:/home/vncbrave/Downloads \
-  -p 5900:5900 \
+  -p 5800:5900 \
   -e WIDTH=1024 -e HEIGHT=768 \
   tenox7/vncbrave:latest
